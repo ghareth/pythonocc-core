526535eac68258dafd8d22db60d9de0c162b6906
Gareth Boyes <garethboyes@MacBook-Pro.local>
2019-06-16 18:32:12 +0100

    Creating setup.py and manifest.in to install Display with pip

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pythonocc',
+    version='0.18.2',
+    description='description to follow',
+    author='Gareth Boyes',
+    author_email='gareth.boyes@gxbis.com',
+    packages=['pythonocc/Display',
+              'pythonocc/Display/WebGl'],
+    package_dir={'pythonocc/Display': 'src/Display',
+                 'pythonocc/Display/WebGl': 'src/Display/WebGl'},
+    include_package_data=True)
